e55b690b98e22a9dc7e16096895d6fc7fcd7f860
Peter Halliday <pghalliday@gmail.com>
2015-11-23 17:02:37 +0100

    don't need ssl for logstash - everything will be inside a private network

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,4 @@ default['formatron_filebeat']['version'] = '1.0.0-rc2'
 default['formatron_filebeat']['checksum'] = 'e16484492ac5b77c2c98ae5d4ba98e45ce3ce6f84ec05ae364680fd0f573f794'
 default['formatron_filebeat']['logstash']['hostname'] = nil
 default['formatron_filebeat']['logstash']['port'] = nil
-default['formatron_filebeat']['logstash']['certificate'] = nil
-default['formatron_filebeat']['logstash']['private_key'] = nil
 default['formatron_filebeat']['paths'] = []
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,35 +3,10 @@ checksum = node['formatron_filebeat']['checksum']
 paths = node['formatron_filebeat']['paths']
 hostname = node['formatron_filebeat']['logstash']['hostname']
 port = node['formatron_filebeat']['logstash']['port']
-certificate = node['formatron_filebeat']['logstash']['certificate']
-private_key = node['formatron_filebeat']['logstash']['private_key']
 
 cache = Chef::Config[:file_cache_path]
 deb = File.join cache, 'filebeat.deb' 
 deb_url = "https://download.elastic.co/beats/filebeat/filebeat_#{version}_amd64.deb"
-
-certificates_dir = '/etc/pki/tls/certs'
-certificate_path = File.join certificates_dir, 'logstash.crt'
-private_keys_dir = '/etc/pki/tls/keys'
-private_key_path = File.join private_keys_dir, 'logstash.key'
-
-directory certificates_dir do
-  recursive true
-end
-
-file certificate_path do
-  content certificate
-  notifies :restart, 'service[filebeat]', :delayed
-end
-
-directory private_keys_dir do
-  recursive true
-end
-
-file private_key_path do
-  content private_key
-  notifies :restart, 'service[filebeat]', :delayed
-end
 
 remote_file deb do
   source deb_url
@@ -49,9 +24,7 @@ template '/etc/filebeat/filebeat.yml' do
   variables(
     paths: paths || [],
     hostname: hostname,
-    port: port,
-    certificate: certificate_path,
-    private_key: private_key_path
+    port: port
   )
   notifies :restart, 'service[filebeat]', :delayed
 end
