cce28f8fe301411e1c6dc6e125780a7492ecd2b5
Peter Halliday <pghalliday@gmail.com>
2015-11-23 16:09:41 +0100

    configuration

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,5 @@ default['formatron_filebeat']['checksum'] = 'e16484492ac5b77c2c98ae5d4ba98e45ce3
 default['formatron_filebeat']['logstash']['hostname'] = nil
 default['formatron_filebeat']['logstash']['port'] = nil
 default['formatron_filebeat']['logstash']['certificate'] = nil
+default['formatron_filebeat']['logstash']['private_key'] = nil
+default['formatron_filebeat']['paths'] = []
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,10 @@
 version = node['formatron_filebeat']['version']
 checksum = node['formatron_filebeat']['checksum']
+paths = node['formatron_filebeat']['paths']
+hostname = node['formatron_filebeat']['logstash']['hostname']
+port = node['formatron_filebeat']['logstash']['port']
+certificate = node['formatron_filebeat']['logstash']['certificate']
+private_key = node['formatron_filebeat']['logstash']['private_key']
 
 cache = Chef::Config[:file_cache_path]
 deb = File.join cache, 'filebeat.deb' 
@@ -14,6 +19,17 @@ end
 dpkg_package 'filebeat' do
   source deb
   action :nothing
+  notifies :restart, 'service[filebeat]', :delayed
+end
+
+template '/etc/filebeat/filebeat.yml' do
+  variables(
+    paths: paths || [],
+    hostname: hostname,
+    port: port,
+    certificate: certificate,
+    private_key: private_key
+  )
   notifies :restart, 'service[filebeat]', :delayed
 end
 
