18e8d1384851b345b92e3b20851d90bee6b9ce6a
Peter Halliday <pghalliday@gmail.com>
2015-12-05 21:28:22 +0100

    oops forgot attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,7 @@
 default['formatron_filebeat']['version'] = nil
 
+default['formatron_filebeat']['enabled'] = true
+
 default['formatron_filebeat']['logstash']['host'] = 'localhost'
 default['formatron_filebeat']['logstash']['port'] = 5044
 default['formatron_filebeat']['paths'] = ['/var/log/**/*.log']
