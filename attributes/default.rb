59a9b41cecb2cfb598b84c269979b7b05913f4b7
Peter Halliday <pghalliday@gmail.com>
2015-12-05 21:31:32 +0100

    start watching logs at multiple levels

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,4 +4,8 @@ default['formatron_filebeat']['enabled'] = true
 
 default['formatron_filebeat']['logstash']['host'] = 'localhost'
 default['formatron_filebeat']['logstash']['port'] = 5044
-default['formatron_filebeat']['paths'] = ['/var/log/**/*.log']
+default['formatron_filebeat']['paths'] = [
+  '/var/log/*.log',
+  '/var/log/*/*.log',
+  '/var/log/*/*/*.log'
+]
